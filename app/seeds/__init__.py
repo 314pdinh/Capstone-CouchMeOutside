2b633321ebac2e97ab6d39c50a8c2207aedbf115
Peter Dinh <314pdinh@gmail.com>
2023-11-30 12:08:26 -0500

    fix seeders, and seeding and models success

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,9 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .groups import seed_groups, undo_groups
+from .groupmembers import seed_groupmembers, undo_groupmembers
+from .activities import seed_activities, undo_activities
+
 
 from app.models.db import db, environment, SCHEMA
 
@@ -16,13 +20,25 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_activities()
+        undo_groupmembers()
+        undo_groups()
         undo_users()
-    seed_users()
+   
+    users = seed_users()
+    print('here are the users-------', users)
+    groups = seed_groups()
+    print('here is the groups ----------', groups)
+    seed_groupmembers(users, groups)
+    seed_activities()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_activities()
+    undo_groupmembers()
+    undo_groups()
     undo_users()
     # Add other undo functions here
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -8,17 +8,17 @@ def seed_users():
     print("Seeding users...")
     
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
+        username='Demo', email='demo@aa.io', password='password', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', bio="Love to travel and looking for buddies who share the same passion! If you're into exploring new places, trying local cuisines, and making unforgettable memories, let's connect.")
     peter = User(
-        username='peter', email='peter@aa.io', password='password1', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
+        username='peter', email='peter@aa.io', password='password1', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', bio="Hey, I'm Peter, travel enthusiast eager to explore new places and meet awesome folks. Coffee chats, spontaneous adventures, or planning the next getaway—I'm up for it all. Let's turn simple moments into unforgettable experiences. Ready for the journey together?")
     josh = User(
-        username='josh', email='josh@aa.io', password='password2', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
+        username='josh', email='josh@aa.io', password='password2', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', bio="Passionate about exploring the world and making connections along the way! If you share a love for travel and meeting interesting souls, let's create some memories together. Coffee, a stroll, or a spontaneous trip—let's make every moment an adventure!")
     emily = User(
-        username='emily', email='emily@aa.io', password='password3', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
+        username='emily', email='emily@aa.io', password='password3', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', bio="Epic journeys and meaningful connections—two of my favorite things! If you're into exploring the world and meeting awesome individuals, let's make it happen. Coffee dates, impromptu meetups, or planning our next big adventure, the possibilities are endless!")
     grace = User(
-        username='grace', email='grace@aa.io', password='password4', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
+        username='grace', email='grace@aa.io', password='password4', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', bio="Exploring the world and eager to connect with fellow travel enthusiasts. If you find joy in discovering new cultures and meeting interesting folks, let's make it happen! Coffee dates, group trips, or simply sharing travel tips—let's turn every moment into an adventure!")
     aiden = User(
-        username='aiden', email='aiden@aa.io', password='password5', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
+        username='aiden', email='aiden@aa.io', password='password5', profile_picture='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', bio="Hey there! I'm Aiden, a travel enthusiast on a mission to explore the world and make meaningful connections. Whether it's swapping travel tips at a cozy cafe or planning our next escapade, I believe in turning every moment into an unforgettable adventure. Let's create memories together!")
 
     db.session.add(demo)
     db.session.add(peter)
