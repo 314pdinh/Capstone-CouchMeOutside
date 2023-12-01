16f760f7c3da08b1e807db45cbb5c6063d100e3a
Peter Dinh <314pdinh@gmail.com>
2023-12-01 10:16:37 -0500

    touchup forms, auth n group routes completed

--- app/forms/activity_form.py
+++ app/forms/activity_form.py
@@ -4,8 +4,8 @@ from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
-class CreateActivityForm(FlaskForm):
+class ActivityForm(FlaskForm):
     activity_name = StringField("Activity Name", validators=[DataRequired()])
     activity_description = StringField('Activity Description', validators=[DataRequired()])
     # activity_image = FileField('activity_image', validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
-    submit = SubmitField('Create Activity')
+    submit = SubmitField('Submit')
